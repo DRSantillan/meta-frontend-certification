065aae7e1049b8a34788f834ca7176eb2499b135
Dean Santillan <dean@Deans-MacBook-Pro.local>
2022-10-05 23:35:34 +1000

    Completed intro to front end dev course and upto week 3 of programming with javascript

---
+++ programming-with-javaScript/week-2/lab-2/objects-arrays.js
@@ -0,0 +1,22 @@
+// Question 1
+var clothes = [];
+// Question 2
+clothes.push('Leather Jacket');
+clothes.push('T-shirt');
+clothes.push('Jeans');
+clothes.push('Shoes');
+clothes.push('Hat');
+// Question 3
+clothes.pop();
+// Question 4
+clothes.push('New Hat');
+// Question 5
+console.log(clothes[2]);
+// Question 6
+var favCar = {};
+// Question 7
+favCar.color = 'Red';
+// Question 8
+favCar.convertible = true;
+// Question 9
+console.log(favCar);
